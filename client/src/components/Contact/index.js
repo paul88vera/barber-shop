99d6a4ede2561693a1de3fccfcda6a29d3961b19
steepllama <steepllama@gmail.com>
2022-04-10 11:17:40 -0500

    Sem worked on the about page and the contact page, created the form and a header for it

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function Contact() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="contact-header">
+        <h2>Contact Us</h2>
+      </div>
+    <form id="contact-form">
+      <div className="form-group">
+        <label>Name</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label>Email</label>
+        <input type="email" className="form-control" />
+        </div>
+      <div className="form-group">
+        <label>Message</label>
+        <textarea className="form-control" rows="5"></textarea>
+      </div>
+        <button type="submit" className="submit-btn">Submit</button>
+    </form>
+    </div>
+  )
 }
 
 export default Contact;
