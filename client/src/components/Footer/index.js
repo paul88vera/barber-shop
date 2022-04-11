81a8890734fdcbd5986ee846a2bd5eb464a30ba6
steepllama <steepllama@gmail.com>
2022-04-10 21:07:54 -0500

    added css to the footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <p>Keep up with us on social media</p>
+      <p className="social-media">Keep up with us on social media</p>
       {/* Are we going to add in social media icons leading to the website? Are we using bootstrap? Is it going to be a sticky footer? */}
-      <br></br>
-      <p>All rights reserved ©2022</p>
+      <p className="rights">All rights reserved ©2022</p>
     </footer>
   );
 }
