d3942ea090152436328aad23997d4de0994de8fa
paul88vera <paul88vera@gmail.com>
2022-04-10 21:54:45 -0500

    added authorization using JWT, changed schemas and server.js for auth changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,12 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type me {
+        _id: ID
+        username: String
+        email: String
+        haircuts: [Haircut]
+    }
     type User {
         _id: ID
         username: String
@@ -17,11 +23,21 @@ const typeDefs = gql`
     }
 
     type Query {
+        me: User
         users: [User]
+        user(username: String!): User
+        haricuts(username: String!): [Haircut]
     }
 
+    type Auth {
+        token: ID!
+        user: User
+      }
+
     type Mutation {
-        addUser(username: String!, email: String!, password: String!): User
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addHaircut(haircutText: String!): Haircut
     }
 `;
 
