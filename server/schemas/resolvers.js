882d4db2b1d5e04645903c0c7f92250b1b89f2ef
John Proodian <93355671+johnproodian@users.noreply.github.com>
2022-04-10 22:06:20 -0500

    Update resolvers.js
    Fixed a typo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,7 @@ const resolvers = {
             if (context.user) {
               const userData = await User.findOne({ _id: context.user._id })
                 .select('-__v -password')
-                .populate('haricuts');
+                .populate('haircuts');
       
               return userData;
             }
