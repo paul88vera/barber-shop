d252f9844354800543c494fbe1b3cb4295175df3
Jotas <jproodian721@gmail.com>
2022-04-11 14:49:52 -0500

    added a user query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,7 +31,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        addHaircut(haircutText: String!): Haircut
+        addHaircut(haircutText: String!, instructions: String): Haircut
     }
 `;
 
