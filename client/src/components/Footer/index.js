c2122fc83f21dc628b1c4d80123cb3ff2aed79b8
steepllama <steepllama@gmail.com>
2022-04-10 20:33:16 -0500

    finished the layout for the footer, still need to center it and drop it to the bottom of the page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <p>Keep up with us on social media</p>
+      {/* Are we going to add in social media icons leading to the website? Are we using bootstrap? Is it going to be a sticky footer? */}
+      <br></br>
+      <p>All rights reserved ©2022</p>
+    </footer>
+  );
 }
 
 export default Footer;
