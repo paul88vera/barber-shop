994b5437261918fc3a58548b50e32c6af984d24e
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-04-15 13:39:00 -0500

    add some styling to Profile cards. Not finished

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Default from '../assets/images/profile-default.png';
+import Button from 'react-bootstrap/Button';
 
 function Profile() {
   return (
@@ -20,7 +21,40 @@ function Profile() {
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={Default} />
           <Card.Body>
-            <Card.Title>Run Buddy</Card.Title>
+            <Card.Title>Buzz Cut</Card.Title>
+            <Card.Text>Details/instructions</Card.Text>
+            <a
+              href="https://young-oasis-06911.herokuapp.com/login"
+              target="_blank"
+            >
+              <Button variant="primary">Edit</Button>
+            </a>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={Default} />
+          <Card.Body>
+            <Card.Title>Pomp</Card.Title>
+            <Card.Text>Details/instructions</Card.Text>
+            <a
+              href="https://young-oasis-06911.herokuapp.com/login"
+              target="_blank"
+            >
+              <Button variant="primary">Edit</Button>
+            </a>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={Default} />
+          <Card.Body>
+            <Card.Title>Biebs</Card.Title>
+            <Card.Text>Details/instructions</Card.Text>
+            <a
+              href="https://young-oasis-06911.herokuapp.com/login"
+              target="_blank"
+            >
+              <Button variant="primary">Edit</Button>
+            </a>
           </Card.Body>
         </Card>
       </div>
