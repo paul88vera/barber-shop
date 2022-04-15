01dc5be247a1a8b118b29fca365e4f19d8d93836
svnday <40030806+svnday@users.noreply.github.com>
2022-04-14 21:13:50 -0500

    border bottom for services

--- client/src/Pages/Services.js
+++ client/src/Pages/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
     return (
         <div className="mx-auto w-75 bg-secondary rounded-3 d-flex justify-content-center m-5">
             <div className="bg-white w-50 text-center m-5 p-5">
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold border-bottom">
                     <strong>
                         Pricing varies between barbers based on experience, these are our starting prices. Please ask your barber for details.
                     </strong>
@@ -18,7 +18,7 @@ const Services = () => {
                 <p className="">
                     Multiple service appointment, these services include a haircut, shave, beard detail, and head shave. Long hair cuts (shoulders or longer) must book the one hour. Head Barbers have experience in multiple aspects of Barbering, including facial hair, straight razor shaves, hair textures, classic styles and techniques. Appointments with a Head Barber are made in hour blocks and have a set price, one client per appointment, multiple services are allowed.
                 </p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         Head Barber Half Hour $38
                     </strong>
@@ -26,7 +26,7 @@ const Services = () => {
                 <p className="">
                     Single service appointment, these services include a haircut, shave, beard detail, head shave, or kiddo. Head Barbers have experience in multiple aspects of Barbering, including facial hair, straight razor shaves, hair textures, classic styles and techniques.
                 </p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                     Cut and Shave $55
                     </strong>
@@ -34,7 +34,7 @@ const Services = () => {
                 <p className="">
                     Tailored haircut and classic straight razor face shave, including hot towel treatment.
                 </p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         Cut and Beard $55
                     </strong>
@@ -42,19 +42,19 @@ const Services = () => {
                 <p className="">
                     Tailored haircut, facial hair sculpting and outlining with a straight razor finish, including hot towel treatment.
                 </p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                     Haircut $30
                     </strong>
                 </h2>
                 <p className="">Tailored cut that suits your needs with one of our experienced barbers, including a straight razor finish</p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>Shave $30</strong>
                 </h2>
                 <p className="">
                     Classic straight razor face shave with hot towel treatment.
                 </p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         Beard Detail $25
                     </strong>
@@ -62,7 +62,7 @@ const Services = () => {
                 <p className="">
                     Sculpting and outlining of facial hair with a straight razor finish, including hot towel treatment.
                 </p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         Buzz $20
                     </strong>
@@ -70,17 +70,17 @@ const Services = () => {
                 <p className="">
                     One length clipper guard all over, includes straight razor finish.
                 </p>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         Kids $30 (Ages 3-12)
                     </strong>
                 </h2>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         Apprentice Services $25
                     </strong>
                 </h2>
-                <h2 className="fw-bold fst-italic">
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         APPOINTMENTS ONLY
                     </strong>
