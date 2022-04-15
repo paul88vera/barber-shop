f824bdce0b46689821cca6eafc358ce6b3441d32
svnday <40030806+svnday@users.noreply.github.com>
2022-04-14 21:11:45 -0500

    branch/julian

--- client/src/Pages/Gallery.js
+++ client/src/Pages/Gallery.js
@@ -3,10 +3,12 @@ import PhotoList from '../components/PhotoList';
 
 function Gallery() {
   return (
-    <section>
-      <h1 data-testid="h1tag">Photos from the Shop</h1>
-      <PhotoList />
-    </section>
+    <div className="mx-auto w-75 text-center">
+      <section>
+        <h1 data-testid="h1tag">Photos from the Shop</h1>
+        <PhotoList />
+      </section>
+    </div>
   );
 }
 
--- client/src/Pages/Services.js
+++ client/src/Pages/Services.js
@@ -1,88 +1,91 @@
 import React from 'react';
-import Calendly from '../components/Calendly';
 
 const Services = () => {
 
     return (
-        <div className="">
-            <Calendly />
-            <div className="">
-                <h3 className="">
+        <div className="mx-auto w-75 bg-secondary rounded-3 d-flex justify-content-center m-5">
+            <div className="bg-white w-50 text-center m-5 p-5">
+                <h2 className="fw-bold fst-italic">
                     <strong>
                         Pricing varies between barbers based on experience, these are our starting prices. Please ask your barber for details.
                     </strong>
-                </h3>
-                <h3 className="">
+                </h2>
+                <h2 className="fw-bold fst-italic border-bottom">
                     <strong>
                         Head Barber Hour $65
                     </strong>
-                </h3>
+                </h2>
                 <p className="">
                     Multiple service appointment, these services include a haircut, shave, beard detail, and head shave. Long hair cuts (shoulders or longer) must book the one hour. Head Barbers have experience in multiple aspects of Barbering, including facial hair, straight razor shaves, hair textures, classic styles and techniques. Appointments with a Head Barber are made in hour blocks and have a set price, one client per appointment, multiple services are allowed.
                 </p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                        Head Barber Half Hour $38
+                    </strong>
+                </h2>
+                <p className="">
+                    Single service appointment, these services include a haircut, shave, beard detail, head shave, or kiddo. Head Barbers have experience in multiple aspects of Barbering, including facial hair, straight razor shaves, hair textures, classic styles and techniques.
+                </p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                    Cut and Shave $55
+                    </strong>
+                </h2>
+                <p className="">
+                    Tailored haircut and classic straight razor face shave, including hot towel treatment.
+                </p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                        Cut and Beard $55
+                    </strong>
+                </h2>
+                <p className="">
+                    Tailored haircut, facial hair sculpting and outlining with a straight razor finish, including hot towel treatment.
+                </p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                    Haircut $30
+                    </strong>
+                </h2>
+                <p className="">Tailored cut that suits your needs with one of our experienced barbers, including a straight razor finish</p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>Shave $30</strong>
+                </h2>
+                <p className="">
+                    Classic straight razor face shave with hot towel treatment.
+                </p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                        Beard Detail $25
+                    </strong>
+                </h2>
+                <p className="">
+                    Sculpting and outlining of facial hair with a straight razor finish, including hot towel treatment.
+                </p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                        Buzz $20
+                    </strong>
+                </h2>
+                <p className="">
+                    One length clipper guard all over, includes straight razor finish.
+                </p>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                        Kids $30 (Ages 3-12)
+                    </strong>
+                </h2>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                        Apprentice Services $25
+                    </strong>
+                </h2>
+                <h2 className="fw-bold fst-italic">
+                    <strong>
+                        APPOINTMENTS ONLY
+                    </strong>
+                </h2>
             </div>
-            <h3 className="">
-                <strong>
-                    Head Barber Half Hour $38
-                </strong>
-            </h3>
-            <p className="">
-                Single service appointment, these services include a haircut, shave, beard detail, head shave, or kiddo. Head Barbers have experience in multiple aspects of Barbering, including facial hair, straight razor shaves, hair textures, classic styles and techniques.
-            </p>
-            <h3 className="">
-                <strong>
-                Cut and Shave $55
-                </strong>
-            </h3>
-            <p className="">
-                Tailored haircut and classic straight razor face shave, including hot towel treatment.
-            </p>
-            <h3 className="">
-                <strong>
-                    Cut and Beard $55
-                </strong>
-            </h3>
-            <p className="">
-                Tailored haircut, facial hair sculpting and outlining with a straight razor finish, including hot towel treatment.
-            </p>
-            <h3 className="">
-                <strong>
-                Haircut $30
-                </strong>
-            </h3>
-            <p className="">Tailored cut that suits your needs with one of our experienced barbers, including a straight razor finish</p>
-            <h3 className="">
-                <strong>Shave $30</strong>
-            </h3>
-            <p className="">
-                Classic straight razor face shave with hot towel treatment.
-            </p>
-            <h3 className="">
-                <strong>
-                    Beard Detail $25
-                </strong>
-            </h3>
-            <p className="">
-                Sculpting and outlining of facial hair with a straight razor finish, including hot towel treatment.
-            </p>
-            <h3 className="">
-                <strong>
-                    Buzz $20
-                </strong>
-            </h3>
-            <p className="">
-                One length clipper guard all over, includes straight razor finish.
-            </p>
-            <h3 className="">
-                <strong>
-                    Kids $30 (Ages 3-12)
-                </strong>
-            </h3>
-            <h3 className="">
-                <strong>
-                    Apprentice Services $25
-                </strong>
-            </h3>
         </div>
     )
 }
