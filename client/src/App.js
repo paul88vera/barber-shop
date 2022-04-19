cb6943bdc2aa506dfa32e5b05c917948644e3ff9
paul88vera <paul88vera@gmail.com>
2022-04-18 22:16:31 -0500

    heroku push 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import { BrowserRouter as Router, Route  } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
