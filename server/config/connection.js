72165a09a38aecd1a6b35788e0fedfe1b9a72844
paul88vera <paul88vera@gmail.com>
2022-04-18 22:20:24 -0500

    heroku 4

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,9 +4,7 @@ mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost:27017/barber-shop',
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+        useUnifiedTopology: true
     }
 );
 
