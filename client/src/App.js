309bf2ea7d7ef34c0d99d9f81fb35489b8bca38a
Jotas <jproodian721@gmail.com>
2022-04-19 21:38:34 -0500

    removed duplicate stuff

--- client/src/App.js
+++ client/src/App.js
@@ -39,15 +39,6 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql'
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
-
 function App() {
   return (
     <ApolloProvider client={client}>
